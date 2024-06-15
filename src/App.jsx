0b18ff436e054c4a6bb505029db2a7be5240b776
msander87 <90715788+msander87@users.noreply.github.com>
2024-06-15 14:03:01 -0300

    adding src files

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [newItem, setNewitem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos(currentTodos=>{
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+
+    setNewitem("")
+  }
+
+  function toogleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if(todo.id === id){
+          return{...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(curre)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewitem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {todos.map(todo => {
+          return <li key={todo.id}>
+          <label>
+            <input type="checkbox" checked={todo.completed} onChange={e => toogleTodo(todo.id, e.target.checked)}/>
+            {todo.title}
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        })}
+        
+        
+      </ul>
+    </>
+  );
+}
